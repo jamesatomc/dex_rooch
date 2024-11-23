5c76599499f0a4199a7745a6b4ab22d36c479895
jamesatomc <jiraphonwansai2002@gmail.com>
2024-11-23 23:32:09 +0700

    Add vite-plugin-vercel to package.json and configure Vite to use it

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
 });
